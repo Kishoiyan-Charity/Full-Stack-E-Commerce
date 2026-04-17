ac83b6f67eeadea4f2152fba3810fd5224088f97
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-04-17 14:56:16 +0300

    prisma and database

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { AuthResponseDto } from './dto/auth-response.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  //register api
+
+  async register(@Body() registerDto: RegisterDto): Promise<AuthResponseDto> {
+
+
+  }
+}
