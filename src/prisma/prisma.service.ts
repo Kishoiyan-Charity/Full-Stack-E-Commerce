917ca4d1c56f916e0eaac7cb81ce4b81cbf872c3
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-04-17 14:55:59 +0300

    prisma and database

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,19 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
     console.log('Database connected successfully!');
@@ -20,9 +28,7 @@ export class PrismaService
     if (process.env.NODE_ENV === 'production') {
       throw new Error('Cannot clean database in production.');
     }
-
     const models = Object.keys(this).filter((key) => !key.startsWith('_'));
-
     return Promise.all(
       models.map((model) => (this as any)[model]?.deleteMany?.()),
     );
