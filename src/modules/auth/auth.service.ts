97b6a73ee08763b61e694d7bd3b7077cb025a53b
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-04-23 11:56:53 +0300

    prisma and database, auth

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { AuthResponseDto } from './dto/auth-response.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly SALT_ROUNDS = 12;
+  constructor(private prisma: PrismaService) {}
+
+  //REGISTER A NEW USER
+  async register(registerDto: RegisterDto): Promise<AuthResponseDto> {
+    const { email, password, firstName, lastName } = registerDto;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('User with this email already exist');
+    }
+    try {
+      const hashedPassword = await bcrypt.hash(password, this.SALT_ROUNDS);
+      const user = await this.prisma.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+          firstName,
+          lastName,
+        },
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          role: true,
+          password: false,
+        },
+      });
+
+      const tokens = await this.generateTokens(user.id, user.email);
+    } catch (_error) {}
+  }
+}
