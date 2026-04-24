1e0a946f713c50dec53150d568726f8bfe82d74d
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-04-24 13:44:37 +0300

    prisma and database

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET') ?? 'defaultsecret2025',
+      }),
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
