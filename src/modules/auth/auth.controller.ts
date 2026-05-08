c43aceee296aa223f05e6c743dbf0822a0b6c347
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-05-08 16:05:01 +0300

    log out

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,30 +1,43 @@
-import { Body, Controller, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { AuthResponseDto } from './dto/auth-response.dto';
 import { AuthService } from './auth.service';
 import { RefreshTokenGuard } from './guards/refresh-token.guard';
 import { GetUser } from '../../common/decorators/get-user.decorators';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   //register api
+  @Post('register')
   async register(@Body() registerDto: RegisterDto): Promise<AuthResponseDto> {
     return await this.authService.register(registerDto);
   }
 
   //refresh access token
+  @Post('refresh')
   @UseGuards(RefreshTokenGuard)
   async refresh(@GetUser('id') userId: string): Promise<AuthResponseDto> {
     return await this.authService.refreshTokens(userId);
   }
 
   // Logout user and invalidate refresh token
+  @Post('logout')
   @UseGuards(JwtAuthGuard)
   async logout(@GetUser('id') userId: string): Promise<{ message: string }> {
     await this.authService.logout(userId);
     return { message: 'Successfully logged out' };
   }
+
+  // Login
+  @Post('login')
+  async login(@Body() loginDto: LoginDto): Promise<AuthResponseDto> {
+    return await this.authService.login(loginDto);
+  }
 }
+
+// REACHED 2:29:06
+// REACHED 2:29:06
