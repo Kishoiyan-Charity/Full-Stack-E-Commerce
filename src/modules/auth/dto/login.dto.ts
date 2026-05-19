4733541df791f5b77a9b7bbaec03491f844f8046
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-05-19 12:31:42 +0300

    auth, and swagger api documentation

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,10 +1,19 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
+  @ApiProperty({
+    description: 'User email address',
+    example: 'kish.yian@gmail.com',
+  })
   @IsEmail({}, { message: 'Please provide an email address' })
   @IsNotEmpty({ message: 'Email address is required' })
   email: string;
 
+  @ApiProperty({
+    description: 'User password',
+    example: 'StrongP@ssw0rd!',
+  })
   @IsString()
   @IsNotEmpty({ message: 'Password is required' })
   password: string;
