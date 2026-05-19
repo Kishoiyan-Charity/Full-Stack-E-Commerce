96ef8c2e4fae2b02122a0c8c07a8a0ca8731e3c2
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-05-19 12:31:21 +0300

    logs, and swagger api documentation

--- src/modules/auth/dto/auth-response.dto.ts
+++ src/modules/auth/dto/auth-response.dto.ts
@@ -1,10 +1,31 @@
 import { Role } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthResponseDto {
+  @ApiProperty({
+    description: 'Access token authentication',
+    example:
+      'eeyydgskspeuyebdnclodieye678GDKSLIV70SDV3Gsujjdleoevddcccdkdcdhdcjhdjd67638h03yy3dhjd993g640RUGRFudkne6630',
+  })
   accessToken: string;
 
+  @ApiProperty({
+    description: 'Access token for obtaining new access token',
+    example:
+      'eeyydgskspeuyebdnclodieye678GDKSbcldbc000dvLIV70SDV3Gsujjdleoevddcccdkdcdhdcjhdjd67638h03yy3dhjd993g640RUGRFudkne6630',
+  })
   refreshToken: string;
 
+  @ApiProperty({
+    description: 'Authenticated user information',
+    example: {
+      id: 'user-123',
+      email: '<Email>',
+      firstname: '<Kishoyan>',
+      lastname: '<Charity>',
+      role: 'USER',
+    },
+  })
   user: {
     id: string;
     email: string;
