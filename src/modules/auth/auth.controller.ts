0933dc121ef7c94e6582b89725f068f60815c0eb
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-05-19 12:31:18 +0300

    logs, and swagger api documentation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { AuthResponseDto } from './dto/auth-response.dto';
 import { AuthService } from './auth.service';
@@ -6,6 +13,7 @@ import { RefreshTokenGuard } from './guards/refresh-token.guard';
 import { GetUser } from '../../common/decorators/get-user.decorators';
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
 import { LoginDto } from './dto/login.dto';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
@@ -13,20 +21,79 @@ export class AuthController {
 
   //register api
   @Post('register')
+  @HttpCode(201)
+  @ApiOperation({
+    summary: 'Register new user',
+    description: 'Creat a new user account',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User Successfully registered',
+    type: AuthResponseDto,
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. Validation failed or user already exists',
+  })
+  @ApiResponse({
+    status: 429,
+    description: 'Too many requests, Rate limit exceeded',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad request. Validation failed or user already exists',
+  })
   async register(@Body() registerDto: RegisterDto): Promise<AuthResponseDto> {
     return await this.authService.register(registerDto);
   }
 
   //refresh access token
   @Post('refresh')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(RefreshTokenGuard)
+  @ApiBearerAuth('JWT-refresh')
+  @ApiOperation({
+    summary: 'Refresh access token',
+    description: 'Generate a new access token using a valid refresh token',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'New access token generated successfully',
+    type: AuthResponseDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. Invalid or expired refresh token',
+  })
+  @ApiResponse({
+    status: 429,
+    description: 'Too many requests, Rate limit exceeded',
+  })
   async refresh(@GetUser('id') userId: string): Promise<AuthResponseDto> {
     return await this.authService.refreshTokens(userId);
   }
 
   // Logout user and invalidate refresh token
   @Post('logout')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('JWT-refresh')
+  @ApiOperation({
+    summary: 'Logout User',
+    description: 'Logout User and invalidate the refresh token',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User successfully logged out',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. Invalid or expired refresh token',
+  })
+  @ApiResponse({
+    status: 429,
+    description: 'Too many requests. Rate limit exceeded',
+  })
   async logout(@GetUser('id') userId: string): Promise<{ message: string }> {
     await this.authService.logout(userId);
     return { message: 'Successfully logged out' };
@@ -34,10 +101,25 @@ export class AuthController {
 
   // Login
   @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'User LogIn',
+    description: 'Authenticate a user and return access and refresh token',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User successfully logged In',
+    type: AuthResponseDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized. Invalid credentials',
+  })
+  @ApiResponse({
+    status: 429,
+    description: 'Too many requests. Rate limit exceeded',
+  })
   async login(@Body() loginDto: LoginDto): Promise<AuthResponseDto> {
     return await this.authService.login(loginDto);
   }
 }
-
-// REACHED 2:29:06
-// REACHED 2:29:06
