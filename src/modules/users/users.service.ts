3c0cd74395d0e5b9ef5da58f39aca6303c1d436d
Kishoyian Charity <kishoiyancharity@gmail.com>
2026-05-20 15:17:49 +0300

    users api routes/ controller/service

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { UserResponseDto } from './dto/user-response.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findOne(userId: string): Promise<UserResponseDto> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+        createdAt: true,
+        updatedAt: true,
+        password: false,
+        role: true,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async findAll();
+}
